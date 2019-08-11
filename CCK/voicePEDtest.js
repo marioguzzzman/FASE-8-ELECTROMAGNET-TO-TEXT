a594bb7afee23eb382ad6068317ceb0c4f57e3af
Mario Guzman <marioguzzzman@gmail.com>
2019-08-12 02:26:08 +0800

    funciona ya la integracion del texto random con el audio, pero aun no logro que funcione el split y el trim con los valores del sensor

--- CCK/voicePEDtest.js
+++ CCK/voicePEDtest.js
@@ -21,24 +21,34 @@
 
 
 //SERIAL
+// https://itp.nyu.edu/physcomp/labs/labs-serial-communication/lab-serial-input-to-the-p5-js-ide/
+
 let serial; // variable to hold an instance of the serialport library
 // var portName = '/dev/cu.usbmodem1421';  // fill in your serial port name here
 let portName = '/dev/ttyACM0';  // For linux 
 // Set Arduino on Serial.begin(9600) to nor change the value of baudrate
+// let portName = '10.17.34.128'; // fill in your own IP address in place of the one shown here
 let currentString;  // for incoming serial data
 
 //LECTURA ARDUINO
 let end = 10;
-let val0, val1, val2, val3, val4, val5;   //numero del valor de EMF 
+let val0;
+let val1;   //numero del valor de EMF 
+
 let serialEMF = []; //valor EMF en String (del Serial)
-// let txt=(random(0, 8)); // Erro: random is not defined 
+let txt = Math.floor(Math.random(0, 8));
+
+let valores = []; //que hace esto?
+
+let openPort; // no se si jalo
+
 
 
 //timer
 let time;
 let wait = 2000;
 let time2;
-let wait2 = 100000;
+let wait2 = 5000; //100000 // que mide este valor?
 
 
 // TEXTOS
@@ -57,25 +67,32 @@ let textos_columnas = [
   "Cielito, ciclo que sí, digo cese la pendencia, ya reventó la coyunda, y viva la Independencia."
 ]; 
 
-let frase, frase2, frase_say;
+
+//RITA
+let rs = new RiString();
+
+let frase = new RiString();
+let frase2 = new RiString();
+let frase_say = new RiString();
+
 let say;
 let count;
 let voiceIndex;
 let voiceSpeed;
 
-// int[] valores; // que hace esto?
+
+valores = []; // que hace esto?
 
 
 //VOICES
-var iptr = 0; // a counter for the words
+let iptr = 0; // a counter for the words - used for TESTING
 
-var myVoice = new p5.Speech(); // new P5.Speech object
+let myVoice = new p5.Speech(); // new P5.Speech object
 let voicesX = ['Google français', 'Google español', 'Google español de Estados Unidos'];
     
 
 function setup() {
 createCanvas(500, 500);
-    // valores = new int [2]; //que hace esto?
 
 // -------------------SERIAL P5js
 
@@ -86,8 +103,8 @@ createCanvas(500, 500);
 //   var portlist = serial.list();
 
   // Assuming our Arduino is connected, let's open the connection to it
-  // Change this to the name of your arduino's serial port
-  serial.open(portName);
+
+  serial.open(portName); // Arduino's serial port
 
   // Register some callbacks
 
@@ -98,7 +115,7 @@ createCanvas(500, 500);
 //   serial.on('list', gotList);
 
   // When we some data from the serial port
-  serial.on('data', gotData);
+  serial.on('data', gotData); // prints = currentString
 
   // When or if we get an error
   serial.on('error', gotError);
@@ -108,6 +125,7 @@ createCanvas(500, 500);
 
 
 // ------------------   SERIAL pre-v
+//CHANGED FOR P5js LIBRARY
 
 //   String portName = Serial.list()[3];
 //   // println(portName);
@@ -117,15 +135,13 @@ createCanvas(500, 500);
 //   serial = null;
 
 //   time = millis();//store the current time
-
 //   time2 = millis();//store the current time
-
 // ------------------   END SERIAL pre-v
 
-
-//TIME
+//TIME //was in setup
 time = millis();//store the current time  // where are these variables?
 time2 = millis();//store the current time // where are these variables?
+// console.log('time: ' + time );
 
 }
 
@@ -138,6 +154,7 @@ function draw() {
 background(0, 50);
   fill(255);
   text("sensor value: " + currentString, 30, 30);
+//   console.log('current string :' + currentString);
 
 
 //PRE-V
@@ -145,38 +162,61 @@ background(0, 50);
 //console.log("draw: " + txt);
 
 //Que hace este codigo?
- 
+
+// console.log('time2: ' + time2);
+
+
   if (millis() - time2 >= wait2) {
     txt = int(random(0, 8));
-    console.log("update: "+txt);
+    console.log("UPDATE: " + txt);
     time2 = millis();//also update the stored time
   }
 
   //LECTURA SERIAL
   /*******************************************************/
-//   if ( port.available() > 0) {  
-//     serial = port.readStringUntil(end);
-//     if (serial != null) {
-//       String[] a = split(serial, ',');
 
-//       val0 =int(float(trim(a[0])));
-//       //println("Col2: "+val0);
+  if (serial.available() > 0) {  //this works
+    var data = serial.read();
+    ellipse(50,50,data,data); // para testear que recibimos data
+    // console.log('Entro a Lectura serial');
 
-//       val1 =int(float(trim(a[1])));
-//       //println("Col3: "+val1);
+    // NO PUEDO HACER QUE FUNCIONE EL SPLIT DE LOS VALORES DEL SENSOR
 
-//       valores[0] = val0;
-//       valores[1] = val1;
-//       println("valores: "+valores[0]+" "+valores[1]);
 
-//       //CODIGO COLUMNA 1 
-//       int columna = int(random(1, 2));
-//       leer_columna(columna, valores[columna]);
-//       //println("col: "+columna);
-//     }
-//   }
+    // if (serial != null) {
+        // String[] a = split(serial, ',');
+
+      let a = currentString.split(",");
+    //   let b = a.split(",");
+    //   a.push(split(currentString, ','));
+
+    text("sensor value 1: " + a, 30, 80);
+    console.log('valores recibidos: ' +  a[0]);
+
+
+    // val0 =int(float(trim(a[0])));
+    // //println("Col2: "+val0);
+
+    // val1 =int(float(trim(a[1])));
+    // //println("Col3: "+val1);
+
+    valores[0] = val0;
+    valores[1] = val1;
+    console.log("valores: "+valores[0]+" "+valores[1]);
+
+    //CODIGO COLUMNA 1 
+    let columna = int(random(1, 2));
+    leer_columna(columna, valores[columna]);
+    //println("col: "+columna);
+    // }
+  }
+
+
+  
 /*******************************************************/
   
+// text("sensor value: " + palabra, 30, 60);
+
 
 
 } // END OF DRAW
@@ -193,88 +233,103 @@ function mousePressed()
 
 
 // cuanto mas campo magnetico menos se entiende
-// String mix_text(RiString rs,int val) {
-//     String palabra="";
-//     int cont = rs.length();
-//     //console.log (cont);
-//     if (val >=30 && val<40) {
-//       palabra = rs.charAt(cont-1);
-//     } else 
-//     if (val>=40 && val <60) {
-//       int ran1 = int(random(cont));
-//       int ran2 = int(random(cont));
-//       if (ran1!= ran2) {
-//         rs.replaceChar((cont-ran1), rs.charAt(ran2));
-//       }
-//       palabra = rs.text();
-//       println(palabra);
-//     }  
-//     if (val >=50) {
-//       rs.insertWord( int(random(5)), comodines[int(random(1, 11))]);
-//       palabra = rs.text();
+function mix_text(rs, val) {
+    let palabra = "";
+    let cont = rs.length();
+    //console.log (cont);
+    if (val >= 30 && val < 40) {
+      palabra = rs.charAt(cont-1);
+    } else 
+    if (val >= 40 && val < 60) {
+       let ran1 = Math.floor(Math.random(cont));
+       let ran2 = Math.floor(Math.random(cont));
+      if (ran1 != ran2) {
+        rs.replaceChar((cont-ran1), rs.charAt(ran2));
+      }
+      palabra = rs.text();
+      console.log(palabra);
+    //   println(palabra);
+
+    }  
+    if (val >=50) {
+      rs.insertWord( Math.floor(Math.random(5)), comodines[Math.floor(Math.random(5))]);
+      palabra = rs.text();
+
+      //revisar corte
+      if (cont>10) {
+        palabra = rs.substring(0, 9);
+      }
+    } 
   
-//       //revisar corte
-//       if (cont>10) {
-//         palabra = rs.substring(0, 9);
-//       }
-//     } 
+    //console.log(palabra);
   
-//     //console.log(palabra);
-  
-//     return palabra;
-//   }
+    return palabra;
+  }
 
 
-//   void leer_columna (int col, int val) {
-//     frase = new RiString(textos_columnas[txt]);
+  function leer_columna(col, val){
+    frase = new RiString(textos_columnas[txt]); //dice una frase de comlumnas partir de un numero random de text
   
-//     if (val>30) {
-//       say = mix_text(frase, val); //llamada a funcion mix_text
-//       voiceSpeed =int(random(30, 200));
-//       frase_say = new RiString(say);
+    if (val > 30) {
+      say = mix_text(frase, val); //llamada a funcion mix_text
+      voiceSpeed = Math.floor(Math.random(30, 200));
+      frase_say = new RiString(say);
       
   
-//       if (frase_say.length()>100) {
-//         say = frase_say.substring(0, 100);
-//       }
-//       wait = int ((frase_say.length()/10)*800);
+      if (frase_say.length()>100) {
+        say = frase_say.substring(0, 100);
+      }
+      wait = frase_say.length()/10 * 800; // porue esta cuenta?
       
-//       //println(wait);
-//     } else {
-//       //reset
-//       reset_columna(col);
-//     }
+      //println(wait);
+    } else {
+      //reset
+      reset_columna(col);
+    }
   
-//     if (millis() - time >= wait) {
-//       TextToSpeech.say(say, TextToSpeech.voices[int(random(1, 5))], voiceSpeed);
-//       println(say);
-//       time = millis();//also update the stored time
-//     }
-//   }
+    //PREVIOS SKETCH --> CHANGE TO P5js SPEECH TO TEXT
+    // if (millis() - time >= wait) {
+    //   TextToSpeech.say(say, TextToSpeech.voices[Math.floor(Math.random(1, 5))], voiceSpeed);
+    //   console.log(say);
+    //   time = millis();//also update the stored time
+    // }
 
-//   void reset_columna(int col) {
-//     say = textos_columnas[txt];
-//     voiceSpeed = 150;
-//     wait = int(random(5000, 10000));
-//   }
+    if (millis() - time >= wait) {
+        myVoice.setVoice(Math.floor(random(voicesX.length)));
+        myVoice.speak(say);
+        // Flata agregarle voice speed
+        console.log(say);
+        time = millis();//also update the stored time
+      }
 
 
-//     // this sends the "say" command to the terminal with the appropriate args
-//     static void say(String script, String voice, int speed) {
-//         try {
-//           Runtime.getRuntime().exec(new String[] {"say", "-v", voice, "[[rate " + speed + "]]" + script});
-//         }
-//         catch (IOException e) {
-//           System.err.println("IOException");
-//         }
-//       }
+
+
+  }
+
+  function reset_columna(col) {
+    say = textos_columnas[txt];
+    voiceSpeed = 150;
+    wait = int(random(5000, 10000));
+  }
+
+
+    // // this sends the "say" command to the terminal with the appropriate args
+    // function say( script,  voice, speed) {
+    //     try {
+    //       Runtime.getRuntime().exec(new String[] {"say", "-v", voice, "[[rate " + speed + "]]" + script});
+    //     }
+    //     catch (IOException e) {
+    //       System.err.println("IOException");
+    //     }
+    //   }
     
-//       // Overload the say method so we can call it with fewer arguments and basic defaults
-//       static void say(String script) {
-//         // 200 seems like a resonable default speed
-//         say(script, MONICA, 200);
-//       }
-//     }
+    //   // Overload the say method so we can call it with fewer arguments and basic defaults
+    //   static void say(String script) {
+    //     // 200 seems like a resonable default speed
+    //     say(script, MONICA, 200);
+    //   }
+    // }
 
 
 
@@ -298,7 +353,7 @@ function gotList(thelist) {
 // There is data available to work with from the serial port
 function gotData() {
     currentString = serial.readStringUntil("\r\n");
-  console.log(currentString);
+//   console.log(currentString);
 }
 
 // Ut oh, here is an error, let's log it
@@ -309,7 +364,8 @@ function gotError(theerror) {
   // Connected to our serial device
 function gotOpen() {
     console.log("Serial Port is open!");
-  }
+    var openPort = new Boolean(true);
+}
 
 // Methods available
 // serial.read() returns a single byte of data (first in the buffer)
